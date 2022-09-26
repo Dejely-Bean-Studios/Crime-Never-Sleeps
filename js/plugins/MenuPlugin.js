e9221b2ae53b6c01ab64cba3a5e24a05c03f9005
Dan4C0D3 <dan.crispin1@gmail.com>
2022-09-26 12:32:51 -0600

    Created test menu

--- js/plugins/MenuPlugin.js
+++ js/plugins/MenuPlugin.js
@@ -8,3 +8,20 @@ Scene_Lockpick_1.prototype.constructor = Scene_Lockpick_1;
 Scene_Lockpick_1.prototype.initialize = function() {
     Scene_MenuBase.prototype.initialize.call(this);
 };
+
+Scene_Lockpick_1.prototype.create = function() {
+    Scene_Lockpick_1.prototype.create.call(this);
+    this._lockpick_window_1 = new Window_Lockpick_1(20, 20, 5000, 4000);
+    this.addWindow(this._lockpick_window_1)
+}
+
+function Window_Lockpick_1() {
+    this.initialize.apply(this, arguments);
+}
+
+Window_Lockpick_1.prototype = Object.create(Window_Base.prototype);
+Window_Lockpick_1.prototype.constructor = Window_Lockpick_1;
+
+Window_Lockpick_1.prototype.initialize = function(x, y, width, hight) {
+    Window_Base.prototype.initialize.call(this, x, y, width, hight);
+}
